90194a2c68d8386a7ddeff7c6b6a0986f73d6c85
LAPTOP-PH1LA8GF\HOAI AN <nhoaian613@gmail.com>
2023-05-12 10:28:09 +0700

    change4

--- src/Layout/DefaultLayout/TradicoreSocial/homeSocial/HomeSocial.jsx
+++ src/Layout/DefaultLayout/TradicoreSocial/homeSocial/HomeSocial.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Feed from '~/Layout/DefaultLayout/HomePage/TradicoreSocial/component/feed/Feed';
-import Sidebar from '~/Layout/DefaultLayout/HomePage/TradicoreSocial/component/sidebar/Sidebar';
-import Rightbar from '~/Layout/DefaultLayout/HomePage/TradicoreSocial/component/rightbar/Rightbar';
-import Navbar from '~/Layout/DefaultLayout/HomePage/TradicoreSocial/component/navbar/Navbar';
+import Feed from '~/Layout/DefaultLayout/TradicoreSocial/component/feed/Feed';
+import Sidebar from '~/Layout/DefaultLayout/TradicoreSocial/component/sidebar/Sidebar';
+import Rightbar from '~/Layout/DefaultLayout/TradicoreSocial/component/rightbar/Rightbar';
+import Navbar from '~/Layout/DefaultLayout/TradicoreSocial/component/navbar/Navbar';
 import './homeSocial.scss';
 
 const HomeSocial = () => {
